f07165ec71efbb64c679300728ecaa7f57d7446f
HolgDer <h.schernewski@variobotic.de>
2025-06-27 13:33:51 +0200

    update structure

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'vyra_base'
 copyright = '2025, Holger Schernewski'
 author = 'Holger Schernewski'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'furo'  # 'alabaster'
-html_static_path = ['_static']
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
@@ -33,12 +13,28 @@ extensions = [
     'sphinx_autodoc_typehints',
 ]
 
+# Autodoc-Einstellungen (optional, aber hilfreich)
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+autodoc_typehints = 'description'  # Typen in der Beschreibung statt Signatur
+
+# Intersphinx-Referenzen
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'rclpy': ("https://docs.ros.org/en/humble/", None),
 }
 
-# Damit dein Code importierbar ist (z. B. src-layout)
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'furo'
+html_static_path = ['_static']
+
+# -- Path Setup (damit src importierbar ist) ---------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../src"))  # oder "../src", je nach Struktur
+sys.path.insert(0, os.path.abspath('../src'))  # ggf. anpassen
