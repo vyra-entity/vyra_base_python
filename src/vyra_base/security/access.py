48f4a543d3aca097158ce7236488b637d47712ef
Holg <h.schernewski@variobotic.de>
2025-07-11 22:04:28 +0200

    update security

--- src/vyra_base/security/access.py
+++ src/vyra_base/security/access.py
@@ -1,19 +1,46 @@
-from pathlib import Path
-from vyra_base.helper.env_handler import EnvHandler
+from enum import Enum
+import uuid
+from typing import Union
 
-class ACCESS_LEVEL:
+from vyra_base.helper.logger import Logger
+from vyra_base.security.trust import (
+    TrustlevelManager, 
+    TRUST_LEVEL, 
+    TRUST_STATUS
+)
+from vyra_base.com.datalayer.interface_factory import remote_callable
+
+class ACCESS_STATUS(Enum, int):
     """
     Enum for access levels.
     """
-    NO_ACCESS = 0
+    NOT_ALLOWED = 0
+    ACCESS_GRANTED = 1
+    MISSING_ACCESS_PARAMETERS = 2
+    INTERNAL_ERROR = 3
+
+    def __repr__(self):
+        pass
+
+class ACCESS_LEVEL(Enum, int):
+    """
+    Enum for access levels.
+    """
     READ_ONLY = 1
     EXTENDED_READ = 2
     CONTROL_AND_PARAM = 3
     EXTENDED_CONTROL = 4
     FULL_ACCESS = 5
 
-    def __repr__(self):
-        pass
+    @staticmethod
+    def parse_access_level(level: int) -> 'ACCESS_LEVEL':
+        """
+        Parse an integer to an ACCESS_LEVEL enum.
+        """
+        try:
+            return ACCESS_LEVEL(level)
+        except ValueError:
+            raise ValueError(f"Invalid access level: {level}")
 
 class AccessManager:
     """ 
@@ -24,8 +51,92 @@ class AccessManager:
         """
         Initialize the AccessManager class.
         """
+        self.tlm = TrustlevelManager()
+
+    @remote_callable
+    def request_access(self,
+                       certificate: str,
+                       module_id: uuid.UUID,
+                       requested_level: Union[int, ACCESS_LEVEL],
+                       level_4_psw: str = None,
+                       level_5_passkey: bytes = None) -> int:
+        """
+        Request access to a module.
+        """
+        if not self.tlm.verify_id(module_id):
+            raise ValueError("Module ID is not verified.")
+
+        if not isinstance(requested_level, (int, ACCESS_LEVEL)):
+            Logger.warn(f"Invalid access level: {requested_level}")
+            raise ValueError(
+                "access_level must be an integer or an ACCESS_LEVEL enum.")
+
+        if not self._check_certificate_signature(certificate):
+            Logger.error(f"Invalid certificate signature for module {module_id}.")
+            return ACCESS_STATUS.NOT_ALLOWED.value()
+
+        func = None
+        args = []
+
+        match ACCESS_LEVEL(requested_level):
+            case ACCESS_LEVEL.READ_ONLY:
+                Logger.info(f"Read-only access granted for module {module_id}.")
+                return ACCESS_STATUS.ACCESS_GRANTED.value()
+            case ACCESS_LEVEL.EXTENDED_READ:
+                Logger.info(f"Extended read access granted for module {module_id}.")
+                return ACCESS_STATUS.ACCESS_GRANTED.value()
+            case ACCESS_LEVEL.CONTROL_AND_PARAM:
+                Logger.info(f"Control and parameter access granted for module {module_id}.")
+                return ACCESS_STATUS.ACCESS_GRANTED.value()
+            case ACCESS_LEVEL.EXTENDED_CONTROL:
+                if level_4_psw is None:
+                    Logger.error("Level 4 password required for extended control access.")
+                    return ACCESS_STATUS.MISSING_ACCESS_PARAMETERS.value()
+                func = self._verify_extended_control
+                args = [level_4_psw]
+            case ACCESS_LEVEL.FULL_ACCESS:
+                if level_4_psw is None:
+                    Logger.error("Level 4 password required for extended control access.")
+                    return ACCESS_STATUS.MISSING_ACCESS_PARAMETERS.value()
+                if level_5_passkey is None:
+                    Logger.error("Level 5 passkey required for extended control access.")
+                    return ACCESS_STATUS.MISSING_ACCESS_PARAMETERS.value()
+                func = self._verify_full_access
+                args = [level_4_psw, level_5_passkey]
+                Logger.info(f"Full access granted for module {module_id}.")
+            case _:
+                Logger.error(f"Invalid access level: {requested_level}")
+                return ACCESS_STATUS.NOT_ALLOWED.value()
+
+        try:            
+            if func(*args):
+                if self.tlm.check_trust_access(module_id) != TRUST_STATUS.SUCCEED:
+                    Logger.error(f"Access denied for module {module_id}.")
+                    return ACCESS_STATUS.NOT_ALLOWED.value()
+                Logger.info(
+                    f"Access granted for module {module_id} "
+                    f"with level {requested_level}.")
+                return ACCESS_STATUS.ACCESS_GRANTED.value()
+            else:
+                Logger.error(
+                    f"Access denied for module {module_id} "
+                    f"with level {requested_level}.")
+                return ACCESS_STATUS.NOT_ALLOWED.value()
+        except Exception as e:
+            Logger.error(f"Error occurred while verifying access for module {module_id}: {e}")
+            return ACCESS_STATUS.INTERNAL_ERROR.value()
+
+    def _verify_extended_control(
+            self, level_4_psw: str) -> bool:
         pass
 
+    def _verify_full_access(
+            self, level_4_psw: str, level_5_passkey: bytes) -> bool:
+        pass
 
-    def blabli(self):
+    def _check_certificate_signature(self, certificate: str) -> bool:
+        """
+        Check the signature of the certificate.
+        This is a placeholder for actual implementation.
+        """
         pass
--- src/vyra_base/security/trust.py
+++ src/vyra_base/security/trust.py
@@ -11,8 +11,8 @@ class TRUST_LEVEL(Enum, int):
     TRUST_ALL = 3
     
 class TRUST_STATUS(Enum, int):
-    TRUSTED = 0
-    UNTRUSTED = 1
+    SUCCEED = 0
+    FAILED = 1
     UNKNOWN = 2
 
 class TrustlevelManager:
@@ -37,7 +37,7 @@ class TrustlevelManager:
         
         return TRUST_LEVEL(eh.env["TRUST_LEVEL"])
 
-    def check_access(self, trust_id, related_ids: list = []) -> TRUST_STATUS:
+    def check_trust_access(self, trust_id, related_ids: list = []) -> TRUST_STATUS:
         """
         Check if the given id has access based on the trust level.
         
@@ -45,31 +45,32 @@ class TrustlevelManager:
         :type id: str
         :return: TRUST_STATUS indicating whether the id is trusted or untrusted.
         """
-        if not self.verify_id(trust_id):        
-            return TRUST_STATUS.UNTRUSTED
+        if not self.verify_id(trust_id):     
+            Logger.warn(f"ID {trust_id} is not valid, trust failed.")   
+            return TRUST_STATUS.FAILED
 
         match self.level:
             case TRUST_LEVEL.TRUST_ALL:
                 if trust_id in self.trusted_ids:
                     Logger.info(f"ID {trust_id} is already trusted.")
-                    return TRUST_STATUS.TRUSTED
-                
+                    return TRUST_STATUS.SUCCEED
+
                 Logger.info(f"ID {trust_id} is added and trusted.")
                 self.trusted_ids.append(trust_id)
-                return TRUST_STATUS.TRUSTED
+                return TRUST_STATUS.SUCCEED
             case TRUST_LEVEL.TRUST_ONE_RESERVE:
                 if len(self.trusted_ids) < 1:
                     Logger.info(f"ID {trust_id} is added and trusted.")
                     self.trusted_ids.append(trust_id)
                 else:
                     Logger.warn(f"Access denied, module already reserved.")
-                    return TRUST_STATUS.UNTRUSTED
+                    return TRUST_STATUS.FAILED
             case TRUST_LEVEL.TRUST_ONE_RELATED:
                 
                 if (len(self.trusted_ids) > 0 and 
                     trust_id not in self.trusted_ids):
                     Logger.warn(f"Access denied, module already reserved.")
-                    return TRUST_STATUS.UNTRUSTED
+                    return TRUST_STATUS.FAILED
                 elif (len(self.trusted_ids) > 0 and 
                     trust_id in self.trusted_ids):
                     Logger.info(f"ID {trust_id} is already trusted.")
@@ -77,14 +78,15 @@ class TrustlevelManager:
                 for related_id in related_ids:
                     if not self.verify_id(related_id):
                         Logger.warn(f"Related ID {related_id} is not valid, access denied.")
-                        return TRUST_STATUS.UNTRUSTED
+                        return TRUST_STATUS.FAILED
                     if related_id not in self.related_ids:
                         Logger.info(f"Related ID {related_id} is added and trusted.")
                         self.related_ids.append((trust_id, related_id))
                     else:
                         Logger.info(f"Related ID {related_id} is already trusted.")
-                
-                return TRUST_STATUS.TRUSTED
+
+                Logger.info(f"ID {trust_id} is added and trusted.")
+                return TRUST_STATUS.SUCCEED
             case _:
                 Logger.warn(f"Unknown trust level {self.level}")
                 return TRUST_STATUS.UNKNOWN
