926d9cfcd449fb1c06cc58a93be42fe072584f58
Holg <h.schernewski@variobotic.de>
2025-11-21 23:28:54 +0100

    bugfix

--- tools/build_auto.sh
+++ tools/build_auto.sh
@@ -9,8 +9,8 @@ if [ $1 == "1" ]; then
     cp dist/vyra_base-0.1.5-py3-none-any.whl ../../VOS2_WORKSPACE/modules/v2_modulemanager_733256b82d6b48a48bc52b5ec73ebfff/wheels
 
 elif [ $1 == "2" ]; then
-    python3 -m pip uninstall vyra_base --break-system-packages -y
-    python3 -m pip install dist/vyra_base-0.1.5-py3-none-any.whl --break-system-packages -y
+    python3 -m pip uninstall vyra_base --break-system-packages
+    python3 -m pip install dist/vyra_base-0.1.5-py3-none-any.whl --break-system-packages
     cp dist/vyra_base-0.1.5-py3-none-any.whl ../../VOS2_WORKSPACE/modules/v2_modulemanager_733256b82d6b48a48bc52b5ec73ebfff/wheels
 fi
 
